debe368077b59425c08c4d849cb5b763b815b3a3
geelona <mr.geelona@gmail.com>
2022-08-07 00:27:25 +0300

    idk

--- main.py
+++ main.py
@@ -12,39 +12,39 @@ circle_count = 10
 circle_list = []
 
 
-class Circle():
-	def __init__(self):
-		self.x = random.randint(0, w);
-		self.y = random.randint(0, h);
-		circle_list.append([self.x, self.y])
-	
-	def draw_circles():
-		pygame.draw.circle(win, (0, 255, 0), (self.x, self.y), 5)
+class Circle:
+    def __init__(self):
+        self.x = random.randint(0, w)
+        self.y = random.randint(0, h)
+        circle_list.append([self.x, self.y])
 
-def draw_bg():
-	win.fill((100, 100, 100))
+    def draw_circles(self):
+        pygame.draw.circle(win, (0, 255, 0), (self.x, self.y), 5)
 
-def draw():
-	draw_bg()
-	pygame.display.update()
 
-#circle-creating--------------------
+# circle-creating--------------------
 def circle_creating():
     for i in range(1, circle_count + 1):
-    	i = Circle()
-    	print(circle_list)
+        Circle()
+
+
 circle_creating()
-#circle-creating--------------------
+# circle-creating--------------------
+
 
 def main():
-	run = True
-	while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-		draw()
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        win.fill((20, 20, 20))
+        pygame.display.update()
+
+        clock.tick(60)
+
 
 if __name__ == "__main__":
-	main()
-
+    main()
